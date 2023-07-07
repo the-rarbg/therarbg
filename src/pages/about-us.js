e80ca5590d68469175b8d6b12f6dbb8804e02dcb
pramodpayme <pramod.singh@paymeindia.in>
2023-07-07 13:33:11 +0530

    movie list done api is calling on scroll

---
+++ src/pages/about-us.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='flex h-screen justify-center items-center'>
+      <p className='text-[6rem] font-bold'>Coming Soon</p>
+    </div>
+  )
+}
+
+export default About;
